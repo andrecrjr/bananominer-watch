0f14e01d9cd484fb20fb82856ffd6fcc91d06086
André Carlos Jr <andre-carlos@live.com>
2020-06-23 22:34:54 -0300

    fixing monkey and remove button

--- scripts/components/renderUser/monkeyAddress.js
+++ scripts/components/renderUser/monkeyAddress.js
@@ -15,18 +15,4 @@ export const monkeyAddress = () => {
   };
 
   return bodyUpdates(insideBody);
-  //   const observeBody = new MutationObserver(() => {
-  //     const banAddress = document.querySelector(".banano__info--address");
-  //     if (banAddress) {
-  //       banAddress.onmouseover = () => {
-  //         console.log("passou por aqui");
-  //       };
-  //     }
-  //   });
-  //   observeBody.observe(body, {
-  //     childList: true,
-  //     subtree: true,
-  //     attributes: true,
-  //     characterData: true,
-  //   });
 };
