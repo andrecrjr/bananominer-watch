0a889bfa61d56b3ae56eac56e8025ba54611749a
sebrock <156824+sebrock@users.noreply.github.com>
2020-07-18 19:30:22 +0200

    Revert "V1.3.0"

--- scripts/components/helper/estimateBan.js
+++
@@ -1,3 +0,0 @@
-export const estimateBananos = (PPD) => {
-  return (2.4 * (PPD / 2) ** 0.44).toFixed(2);
-};
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,4 @@
 import { renderUserData } from "./components/renderUser/index.js";
-import { renderTop } from "./components/renderTop/index.js";
 (function () {
-  renderTop();
   renderUserData();
 })();
